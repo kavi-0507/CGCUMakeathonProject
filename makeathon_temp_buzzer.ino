722b3baf46bf7acb4935e94f425fea3cc88d10c2
Kavi Arora <60694177+kavi-0507@users.noreply.github.com>
2026-02-21 18:24:00 +0000

    Add files via upload

---
+++ makeathon_temp_buzzer.ino
@@ -0,0 +1,41 @@
+#define DHT_PIN     4
+#define DHT_TYPE    DHT11
+#define BUZZER_PIN  26
+#define TEMP_DANGER 30.0
+
+#include <DHT.h>
+DHT dht(DHT_PIN, DHT_TYPE);
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+  pinMode(BUZZER_PIN, OUTPUT);
+  
+  // test tone on startup
+  tone(BUZZER_PIN, 1000, 2000);
+  delay(2000);
+}
+
+void loop() {
+  float temp = dht.readTemperature();
+
+  if (isnan(temp)) {
+    Serial.println("Sensor error!");
+    return;
+  }
+
+  Serial.printf("Temp: %.1f°C\n", temp);
+
+  if (temp < TEMP_DANGER) {
+    tone(BUZZER_PIN, 1000, 500);
+    delay(600);
+    tone(BUZZER_PIN, 1000, 500);
+    delay(600);
+    tone(BUZZER_PIN, 1000, 500);
+    delay(1000);
+  } else {
+    noTone(BUZZER_PIN);
+  }
+
+  delay(2000);
+}
